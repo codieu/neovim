f6e74c7848dfea1d6dbc16289ffa62f4dd5fb057
codie <codieuwu@gmail.com>
2025-01-12 23:33:11 +0300

    fixed colorscheme

--- lua/codie/plugs/rosepink.lua
+++ lua/codie/plugs/rosepink.lua
@@ -3,8 +3,14 @@ local M = {
 	name = "rose-pine",
 
 	config = function()
-		vim.cmd("colorscheme rose-pine-moon")
-	end
+		vim.cmd("colorscheme rose-pine")
+	end,
+
+  opts = {
+    variant = "auto", -- auto, main, moon, or dawn
+    dark_variant = "moon", -- main, moon, or dawn
+    dim_inactive_windows = true,
+  }
 }
 
 return M
